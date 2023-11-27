1fd338e2e1524d484a1e260494f32e03acbdbd4b
christiangh01 <cjgh0110@gmail.com>
2023-11-26 23:58:06 -0500

    agrego comentario en Main de POOGAME

--- POO_Game/src/Main.java
+++ POO_Game/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        //EDITADO POR CHRISTIAN GODOY
 
         Toyota toyo = new Toyota("PAS201"); //200
         toyo.encenderMotor();
